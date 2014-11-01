e166718519b786b53188722e3baeab525b17df1f
Will Morgan <me@willmorgan.co.uk>
2014-11-01 16:16:02 +0000

    Cleaned up the code a bit, using SCUpload

---
+++ www/app/SCUpload.php
@@ -0,0 +1,140 @@
+<?php
+
+/**
+ * SCUpload
+ * @author @willmorgan
+ */
+
+use Slim\Slim;
+use Njasm\Soundcloud;
+
+class SCUpload extends Slim {
+
+	/**
+	 * Constructor settings
+	 * @var array
+	 */
+	protected $sc_settings = array();
+
+	/**
+	 * @var array
+	 */
+	public $run_config;
+
+	/**
+	 * @var array
+	 */
+	public $app_config;
+
+	/**
+	 * @var string
+	 */
+	public $oauth_token;
+
+	/**
+	 * @param array $settings Must contain run_config and app_config params
+	 */
+	public function __construct(array $settings) {
+		parent::__construct();
+		// Back up the settings (may be used later)
+		$this->sc_settings = $settings;
+		// Load the config files: config is semi-portable;
+		// run_config should not be copied across instances
+		$this->loadConfig('run_config', $settings['run_config']);
+		$this->loadConfig('app_config', $settings['app_config']);
+
+		// Check if we have an oauth token.
+		$this->oauth_token = $this->run_config['soundcloud']['oauth_token'];
+	}
+
+	/**
+	 * Load a config file's JSON into the app
+	 * @param string $kind
+	 * @param string $file The JSON file
+	 * @return $this
+	 */
+	public function loadConfig($kind, $file) {
+		$decoded = json_decode(file_get_contents($file), true);
+		if(is_null($decoded)) {
+			throw new InvalidArgumentException(
+				'Could not load config file, maybe ' . $file . ' is malformed'
+			);
+		}
+		$this->$kind = $decoded;
+		return $this;
+	}
+
+	/**
+	 * The token must be valid and must belong to the right user
+	 * specified on the config file
+	 * @param string $token The token to use if we're testing a new one, instead of one held in the app
+	 * @return boolean
+	 */
+	public function isTokenValid($token = null) {
+		if(!isset($token)) {
+			$token = $this->oauth_token;
+		}
+		if(empty($token)) {
+			return false;
+		}
+		$sc = $this->getSoundcloud();
+		$sc->setAccessToken($token);
+		$meResponse = $sc->get('/me')->asJson()->request()->bodyObject();
+		$tokenUser = $meResponse->username;
+		$allowedUser = $this->app_config['settings']['soundcloud_user'];
+		return $tokenUser == $allowedUser;
+	}
+
+	/**
+	 * Validate the token to make sure that it's valid and belongs to
+	 * the right user in the settings. Don't want to upload to someone
+	 * else's account!
+	 * @param string $code Should be from the redirect URI's GET params
+	 * @throws LogicException
+	 * @return boolean
+	 */
+	public function setTokenFromOAuthCode($code) {
+		$sc = $this->getSoundcloud();
+		$scConfig = $this->app_config['credentials']['soundcloud'];
+		$response = $sc->codeForToken($code)->bodyObject();
+		if(empty($response->access_token) || strpos($response->scope, 'non-expiring') === false) {
+			throw new LogicException('Scope must be non-expiring, or no access token response');
+		}
+		$token = $response->access_token;
+		if(!$this->isTokenValid($token)) {
+			throw new LogicException('Logged in as the wrong user');
+		}
+		$this->saveOAuthToken($token);
+		return true;
+	}
+
+	/**
+	 * Persist the OAuth token to our runtime configuration
+	 * Check its validity with $this->isTokenValid
+	 * @param string $token
+	 * @return $this
+	 */
+	public function saveOAuthToken($token) {
+		$this->run_config['soundcloud']['oauth_token'] = $token;
+		file_put_contents(
+			$this->sc_settings['run_config'],
+			json_encode($this->run_config)
+		);
+		return $this;
+	}
+
+	/**
+	 * Make me a Soundcloud facade with a hardcoded port name in the callback URL
+	 * @return Njasm\Soundcloud\Soundcloud\SoundcloudFacade
+	 */
+	public function getSoundcloud() {
+		$scConfig = $this->app_config['credentials']['soundcloud'];
+		$redirectURI = 'http://' . $this->app_config['settings']['server'] . '/oauth2callback';
+		return new Soundcloud\SoundcloudFacade(
+			$scConfig['client_id'],
+			$scConfig['client_secret'],
+			$redirectURI
+		);
+	}
+
+}
--- www/index.php
+++ www/index.php
@@ -6,59 +6,19 @@
  * So we have to implement it in code (-1 hour of my life!!)
  * @author @willmorgan
  */
+
 include 'vendor/autoload.php';
+include 'app/SCUpload.php';
 
 use Slim\Slim;
 use Njasm\Soundcloud;
 
-
-/**
- * Go and make me a SoundCloud facade with a hardcoded port name in the callback URL
- * @return Njasm\Soundcloud\Soundcloud\SoundcloudFacade
- */
-function ugly_make_sc_facade($app) {
-	$scConfig = $app->app_config['credentials']['soundcloud'];
-	$serverAndPort = $_SERVER['SERVER_NAME'] . ':8080';
-	$redirectURI = 'http://' . $serverAndPort . '/oauth2callback';
-	return new Soundcloud\SoundcloudFacade(
-		$scConfig['client_id'],
-		$scConfig['client_secret'],
-		$redirectURI
-	);
-};
-
-/**
- * The token must be valid and must belong to the right user
- * specified on the config file
- * @param Slim\Slim $app
- * @param string $token The token to use if we're testing a new one, instead of one held in the app
- * @return boolean
- */
-function ugly_is_the_token_valid($app, $token = null) {
-	if(!isset($token)) {
-		$token = $app->oauth_token;
-	}
-	if(empty($token)) {
-		return false;
-	}
-	$sc = ugly_make_sc_facade($app);
-	$sc->setAccessToken($token);
-	$meResponse = $sc->get('/me')->asJson()->request()->bodyObject();
-	$tokenUser = $meResponse->username;
-	$allowedUser = $app->app_config['settings']['soundcloud_user'];
-	return $tokenUser == $allowedUser;
-};
-
 echo '<pre>';
 
-$app = new Slim();
-
-// Load the config files: config is semi-portable, but run_config should not be copied across instances
-$app->run_config = json_decode(file_get_contents('run_config.json'), true);
-$app->app_config = json_decode(file_get_contents('config.json'), true);
-
-// Check if we have an oauth token.
-$app->oauth_token = $app->run_config['soundcloud']['oauth_token'];
+$app = new SCUpload(array(
+	'run_config' => './run_config.json',
+	'app_config' => './config.json',
+));
 
 /**
  *
@@ -72,8 +32,10 @@ $app->oauth_token = $app->run_config['soundcloud']['oauth_token'];
  */
 $app->get('/', function() use($app) {
 	$checks = array(
+		'app_config_is_readable' => file_exists('config.json'),
+		'run_config_is_readable' => file_exists('run_config.json'),
 		'run_config_is_writable' => is_writable('run_config.json'),
-		'has_valid_oauth_token' => ugly_is_the_token_valid($app),
+		'has_valid_oauth_token' => $app->isTokenValid(),
 	);
 	echo '<h3>Instance status</h3>';
 	echo '<ul>';
@@ -88,8 +50,7 @@ $app->get('/', function() use($app) {
  * Kinda a waste of a route but oh well
  */
 $app->get('/get-token', function() use ($app) {
-	$sc = ugly_make_sc_facade($app);
-	$link = $sc->getAuthURL();
+	$link = $app->getSoundcloud()->getAuthURL();
 	echo '<a href="'.$link.'">Authenticate</a>';
 });
 
@@ -100,27 +61,12 @@ $app->get('/get-token', function() use ($app) {
  * I LOVE OAUTH IT'S SO GREAT
  */
 $app->get('/oauth2callback', function() use($app) {
-	$sc = ugly_make_sc_facade($app);
-	$scConfig = $app->app_config['credentials']['soundcloud'];
-	$request = $app->request;
-	$code = $app->request->get('code');
-	$response = $sc->codeForToken($code)->bodyObject();
 	try {
-		if(empty($response->access_token) || strpos($response->scope, 'non-expiring') === false) {
-			throw new LogicException('Scope must be non-expiring, or no access token response');
-		}
-		$token = $response->access_token;
-		$sc->setAccessToken($token);
-		if(!ugly_is_the_token_valid($app, $token)) {
-			throw new LogicException('Logged in as the wrong user');
-		}
-		$app->run_config['soundcloud']['oauth_token'] = $token;
-		file_put_contents('run_config.json', json_encode($app->run_config));
-		echo 'Runtime config updated, you can now proceed to run the upload task!';
+		$app->setTokenFromOAuthCode($app->request->get('code'));
+		echo 'Token successfully obtained and saved, you may now continue to run the task';
 	}
-	catch(LogicException $e) {
+	catch(Exception $e) {
 		echo 'Error occurred ('.$e->getMessage().'). <a href="/get-token">Start again</a>?<br>';
-		var_dump($response);
 	}
 });
 
