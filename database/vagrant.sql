225aa1e07f25cdad95c237462ddad48a074385b7
Will Morgan <me@willmorgan.co.uk>
2014-11-23 23:05:24 +0000

    Queue server config

--- database/vagrant.sql
+++ database/vagrant.sql
@@ -1,2 +1,14 @@
 CREATE DATABASE  IF NOT EXISTS `vagrant` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
 USE `vagrant`;
+
+DROP TABLE IF EXISTS `scupload_track_queue`;
+
+CREATE TABLE `scupload_track_queue` (
+	id varchar(64) primary key,
+	coalesce_id varchar(40),
+	sequence int(11) auto_increment unique,
+	mutex tinyint(1) not null default "0",
+	state varchar(255) not null,
+	object BLOB not null
+) ENGINE=InnoDB AUTO_INCREMENT=1;
