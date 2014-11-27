ec479d0cd7c86e2758b2dd4a8adc0e61a45daeb5
Will Morgan <me@willmorgan.co.uk>
2014-11-27 22:02:18 +0000

    Fix database for proper coaslesce IDs

--- database/vagrant.sql
+++ database/vagrant.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS `scupload_track_queue`;
 
 CREATE TABLE `scupload_track_queue` (
 	id varchar(64) primary key,
-	coalesce_id varchar(40),
+	coalesce_id varchar(128),
 	sequence int(11) auto_increment unique,
 	mutex tinyint(1) not null default "0",
 	state varchar(255) not null,
